09bedc6a6712aa63763a0a9cfa80b50e51130453
Makiah Bennett <makiahtbennett@gmail.com>
2019-08-15 11:46:29 -0400

    Does pretty much everything I intend it to

--- contentScript.js
+++ contentScript.js
@@ -30,26 +30,63 @@ function updateListings()
             var url = liItem.children[0].href;
             var fixedUrl = url.substring(0, url.lastIndexOf('/') + 1);
 
+            var alreadyApplied = appliedUrls.includes(fixedUrl);
+            if (alreadyApplied)
+                liItem.style["background-color"] = "green";
+            else 
+                liItem.style["background-color"] = "initial";
+
             // Should just have two without button
             if (liItem.children.length < 3)
             {
                 var button = document.createElement('input');
                 button.type = "button";
                 button.name = "name";
-                button.value = "value";
+                button.value = (alreadyApplied ? "Applied" : "Not Applied");
                 button.id = "id";
+                button.style["background-color"] = "rgb(200, 200, 200)";
+                button.linkedInUrl = fixedUrl;
+                button.liItem = liItem;
                 button.onclick = function() 
                 {
+                    console.log("Got onclick for " + this.linkedInUrl);
+                    var linkedInUrl = this.linkedInUrl;
+                    var thisLiItem = this.liItem;
+                    var thisButton = this;
+
                     chrome.storage.sync.get(['activeUrls'], function(result) 
                     {
-                        appliedUrls = result.activeUrls || [];
+                        var appliedUrls = result.activeUrls || [];
+                        console.log("Applied URLs", appliedUrls);
+
+                        var index = appliedUrls.indexOf(linkedInUrl);
+                        if (index > -1)
+                        {
+                            // Toggle to not applied
+                            thisButton.value = "Not Applied";
+
+                            appliedUrls.splice(index, 1);
+                            console.log("Spliced out");
+
+                            thisLiItem.style["background-color"] = "initial";
+
+                            chrome.storage.sync.set({activeUrls: appliedUrls});
+                        }
+                        else
+                        {
+                            thisButton.value = "Applied";
+
+                            appliedUrls.push(linkedInUrl);
+                            console.log("Pushed in");
+
+                            thisLiItem.style["background-color"] = "green";
+
+                            chrome.storage.sync.set({activeUrls: appliedUrls});
+                        }
                     });
                 }
                 liItem.appendChild(button);
             }
-
-            if (appliedUrls.includes(fixedUrl))
-                liItem.style["background-color"] = "green";
         }
     });
 }
@@ -58,6 +95,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     if (request.action == "updatelisting")
     {
+        observer = null;
         updateListings();
     }
 });
