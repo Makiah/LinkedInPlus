8271ecbd72f45ef5fafe1244a295296e4ca10e75
Makiah Bennett <makiahtbennett@gmail.com>
2019-08-15 01:01:19 -0400

    I always make this more complicated than it needs to be but it's fun lol

---
+++ background.js
@@ -0,0 +1,17 @@
+// For SPA
+chrome.webNavigation.onHistoryStateUpdated.addListener(function()
+{
+    console.log("update history state");
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) 
+    {
+        if (tabs[0].url === "https://www.linkedin.com/jobs/saved/")
+        {
+            console.log("it's the right one");
+            setTimeout(() => chrome.tabs.sendMessage(tabs[0].id, 
+            {
+                action: "updatelisting"
+            }), 
+            500);
+        }
+    });
+});
--- contentScript.js
+++ contentScript.js
@@ -17,8 +17,6 @@ function updateListings()
     });
 }
 
-updateListings();
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) 
 {
     console.log("Update listings");
